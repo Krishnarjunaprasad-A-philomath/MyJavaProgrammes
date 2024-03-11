cb461135eb32c61954edc2d0922fbe8b34fb111b
Krishnarjuna Prasad Desilinka <156991911+Krishnarjunaprasad-A-philomath@users.noreply.github.com>
2024-03-11 10:35:56 +0530

    Add files via upload

---
+++ Matric Addition.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int A[][]={{3,5,9},{6,7,2},{9,5,7}};
+        int B[][]={{4,5,2},{7,8,4},{8,4,9}};
+        int c[][]=new int[3][3];
+        for(int i=0;i<A.length;i++){
+            for(int j=0;j< A.length;j++){
+                c[i][j]=A[i][j]+B[i][j];
+            }
+        for(int x[]:c){
+            for(int y:x){
+                System.out.println(y+" ");
+            }
+            System.out.println(" ");
+        }
+        }
+    }
+}
